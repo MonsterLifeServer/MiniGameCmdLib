2323409eded5a32139727f7d1d0e04f8dc173ec3
Monster2408 <meoto2408@gmail.com>
2024-10-07 05:47:17 +0900

    いらんコマンド実装してた。fill面倒だから消そうかな...(setblockは実装しちゃった)

--- src/main/java/xyz/mlserver/miniGameCmdLib/MiniGameCmdLib.java
+++ src/main/java/xyz/mlserver/miniGameCmdLib/MiniGameCmdLib.java
@@ -1,13 +1,31 @@
 package xyz.mlserver.miniGameCmdLib;
 
 import org.bukkit.plugin.java.JavaPlugin;
-import xyz.mlserver.miniGameCmdLib.cmds.MiniGameCmdLibCommand;
+import xyz.mlserver.mc.util.CustomConfiguration;
+import xyz.mlserver.miniGameCmdLib.cmds.FillCmd;
+import xyz.mlserver.miniGameCmdLib.cmds.SetBlockCmd;
+
+import java.util.Objects;
 
 public final class MiniGameCmdLib extends JavaPlugin {
 
+    public static CustomConfiguration config;
+
+    private static String commandWorld = null;
+
+    public static String getCommandWorld() {
+        return commandWorld;
+    }
+
     @Override
     public void onEnable() {
-        getCommand("mgcl").setExecutor(new MiniGameCmdLibCommand());
+        config = new CustomConfiguration(this, "config.yml");
+        config.saveDefaultConfig();
+
+        if (config.getConfig().get("command-world") != null) commandWorld = config.getConfig().getString("command-world");
+
+        Objects.requireNonNull(getCommand("set_block")).setExecutor(new SetBlockCmd());
+        Objects.requireNonNull(getCommand("fill2")).setExecutor(new FillCmd());
     }
 
     @Override
---
+++ src/main/java/xyz/mlserver/miniGameCmdLib/cmds/FillCmd.java
@@ -0,0 +1,65 @@
+package xyz.mlserver.miniGameCmdLib.cmds;
+
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.block.CommandBlock;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+
+public class FillCmd implements CommandExecutor {
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String cmdLabel, @NotNull String[] args) {
+        Component message;
+        if (args.length >= 4) {
+            try {
+                String worldName;
+                if (sender instanceof Player) {
+                    worldName = ((Player) sender).getWorld().getName();
+                } else if (sender instanceof CommandBlock) {
+                    worldName = ((CommandBlock) sender).getWorld().getName();
+                } else {
+                    if (args.length < 5) {
+                        message = Component.text()
+                                .append(Component.text("Usage: /fill2 <Material> <x1> <y2> <z3> <x1> <y2> <z3> <world>").color(NamedTextColor.GRAY))
+                                .build();
+                        sender.sendMessage(message);
+                        return true;
+                    }
+                    worldName = args[4];
+                }
+                Material material = Material.matchMaterial(args[1]);
+                if (material == null) {
+                    message = Component.text()
+                            .append(Component.text("Invalid material! ").color(NamedTextColor.RED))
+                            .build();
+                    sender.sendMessage(message);
+                    message = Component.text()
+                            .append(Component.text("Usage: /set_block <Material> <x> <y> <z> [<world>]").color(NamedTextColor.GRAY))
+                            .build();
+                    sender.sendMessage(message);
+                    return true;
+                }
+                Location loc = new Location(sender.getServer().getWorld(worldName), Double.parseDouble(args[1]), Double.parseDouble(args[2]), Double.parseDouble(args[3]));
+                loc.getBlock().setType(material);
+            } catch (NumberFormatException e) {
+                message = Component.text()
+                        .append(Component.text("Invalid number format! ").color(NamedTextColor.RED))
+                        .build();
+                sender.sendMessage(message);
+                message = Component.text()
+                        .append(Component.text("Usage: /set_block <Material> <x> <y> <z> [<world>]").color(NamedTextColor.GRAY))
+                        .build();
+                sender.sendMessage(message);
+            }
+            return true;
+        }
+        return false;
+    }
+
+}
